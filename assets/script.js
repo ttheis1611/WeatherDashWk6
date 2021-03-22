c384f52387c43207f8d806e7fbb4e8e5ec6bda7f
Terry Theis <tltheis@prodigy.net>
2021-03-22 14:02:49 -0500

    get weather

--- assets/script.js
+++ assets/script.js
@@ -20,3 +20,22 @@ function clearCity() {
     localStorage.clear();
     location.reload();
 };
+
+
+
+function getWeather(desiredCity) {
+    var queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${desiredCity}&APPID=${apiKey}&units=imperial`;
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    })
+    .then(function(weatherData) {
+        var cityObj = {
+            cityName: weatherData.name,
+            cityTemp: weatherData.main.temp,
+            cityHumidity: weatherData.main.humidity,
+            cityWindSpeed: weatherData.wind.speed,
+            cityWindDirect: weatherData.wind.deg,
+            cityUVIndex: weatherData.coord,
+            cityWeatherIconName: weatherData.weather[0].icon
+        }
